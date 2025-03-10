4db4f7068df2fe3e653308db7b2785dfc5a3b780
Aldonis12 <micklewis.aldonis@gmail.com>
2025-03-10 20:40:39 +0300

    Rendez-vous

--- server.js
+++ server.js
@@ -2,11 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const roleRoutes = require('./src/routes/roles');
-const userRoutes = require('./src/routes/users');
 const roleAuth = require('./src/routes/auth');
-const typeVehiculeRoutes = require('./src/routes/typevehicule');
-const vehiculeRoutes = require('./src/routes/vehicules');
 
 dotenv.config();
 const app = express();
@@ -20,10 +16,8 @@ mongoose.connect(process.env.MONGO_URI, {
   .catch(err => console.error(err));
   
 app.use('/login', roleAuth);
-app.use('/roles', roleRoutes);
-app.use('/users', userRoutes);
-app.use('/typevehicule', typeVehiculeRoutes);
-app.use('/vehicules', vehiculeRoutes);
+app.use('/prestations', require('./src/routes/prestationRoute'))
+app.use('/avis', require('./src/routes/avisRoute'))
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Serveur sur http://localhost:${PORT}`));
