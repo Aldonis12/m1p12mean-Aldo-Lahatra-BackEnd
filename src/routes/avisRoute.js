3d217128cc6be5d27242cc13e9de24b7b7403d5c
lahatrazo <lahatrafate@gmail.com>
2025-03-21 17:36:02 +0300

    begin pagination

--- src/routes/avisRoute.js
+++ src/routes/avisRoute.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { addAvis } = require('../controllers/AvisController');
+const authGuard = require('../middlewares/authGuard');
 const router = express.Router();
 
-router.post('/', addAvis)
+router.post('/', authGuard ,addAvis)
 
 module.exports = router;
