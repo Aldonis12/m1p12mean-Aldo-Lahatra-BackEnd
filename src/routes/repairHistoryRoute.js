bcaad1025740d08f1349e2685e9ef5a64ae4424f
lahatrazo <lahatrafate@gmail.com>
2025-03-23 18:03:15 +0300

    little brush up

--- src/routes/repairHistoryRoute.js
+++ src/routes/repairHistoryRoute.js
@@ -9,22 +9,4 @@ router.get('/', authGuard, getAllRepairHistory);
 
 router.get('/mecanicien/:mecanicienId', getRepairHistoryForMecanicien)
 
-router.get('/products', async (req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-
-    const startIndex = (page - 1) * limit;
-    const total = await Product.countDocuments();
-
-    const products = await Product.find().skip(startIndex).limit(limit);
-
-    res.json({
-        page,
-        limit,
-        total,
-        pages: Math.ceil(total / limit),
-        data: products
-    });
-});
-
 module.exports = router;
